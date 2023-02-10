aee2447da4a672b254cb21e972722d304ca5a00c
Prince Roy <princeku.ray143@gmail.com>
2023-02-11 01:18:50 +0530

    import images dynamically

--- src/components/jobComponent/JobComponent.jsx
+++ src/components/jobComponent/JobComponent.jsx
@@ -2,14 +2,13 @@ import Company from '../companyComponent/CompanyComponent';
 import Criteria from '../criteriaComponent/CriteriaComponent';
 import Tags from '../tagsComponent/TagsComponent';
 
-import logo from '../../assets/images/insure.svg';
+import icons from '../../icons';
 
 import './JobComponentStyles.scss';
 
 const Job = ({ companyData }) => {
 	const {
 		company,
-		//logo,
 		new: newAdded,
 		featured,
 		position,
@@ -22,12 +21,14 @@ const Job = ({ companyData }) => {
 		tools,
 	} = companyData;
 
+	const formatName = name => name.toLowerCase().replaceAll(' ', '_').replaceAll('.', '');
+
 	const mentionedTags = [role, level, ...languages, ...tools];
 
 	return (
 		<div className='Job'>
 			<div>
-				<img src={logo} alt=''></img>
+				<img src={icons[formatName(company)]} alt=''></img>
 			</div>
 			<div className='Job__Description'>
 				<Company companyName={company} newAdded={newAdded} featured={featured}></Company>
---
+++ src/icons.js
@@ -0,0 +1,24 @@
+import photosnap from './assets/images/photosnap.svg';
+import faceit from './assets/images/faceit.svg';
+import myhome from './assets/images/myhome.svg';
+import manage from './assets/images/manage.svg';
+import insure from './assets/images/insure.svg';
+import eyecam_co from './assets/images/eyecam-co.svg';
+import loop_studios from './assets/images/loop-studios.svg';
+import shortly from './assets/images/shortly.svg';
+import the_air_filter_company from './assets/images/the-air-filter-company.svg';
+import account from './assets/images/account.svg';
+
+const icons = {
+	photosnap,
+	faceit,
+	myhome,
+	manage,
+	insure,
+	eyecam_co,
+	loop_studios,
+	shortly,
+	account,
+	the_air_filter_company,
+};
+export default icons;
